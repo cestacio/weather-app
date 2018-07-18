5a37d2fff3eebe7f45565ef78de37ba890aad499
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-07-18 15:58:41 -0700

    add Form and Weather component and API key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+
 import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = 'bc4537db67dbdcefd7d1623251b735a5';
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/find?q=Seattle,us&appid=${API_KEY},&units=imperial`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
   render() {
     return (
       <div>
-        <p>Hello!</p>
-        <h1>Yo!</h1>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
