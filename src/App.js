a0d05c59600227a06c2a57f1636dd21d5254a61d
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-07-18 18:33:05 -0700

    change Form, Weather, and Titles stateful components to stateless components

--- src/App.js
+++ src/App.js
@@ -7,19 +7,58 @@ import Weather from './components/Weather';
 const API_KEY = 'bc4537db67dbdcefd7d1623251b735a5';
 
 class App extends React.Component {
-  getWeather = async () => {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/find?q=Seattle,us&appid=${API_KEY},&units=imperial`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`
     );
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please out the form.'
+      });
+    }
   };
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
